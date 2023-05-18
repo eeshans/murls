a2545b6a07a9a40ca9913fc6ed5803d79f0ab4ef
Eeshan S <esathaye@dev1-ubuntu-20.04>
2023-05-18 15:09:47 -0500

    Changing main function to select and sort urls.

--- malicious-urls-reader.py
+++ malicious-urls-reader.py
@@ -1,7 +1,6 @@
 # imports
 import pandas as pd
 
-
 # reading file
 def fileread():
     pd.options.display.max_rows = 50
@@ -11,27 +10,40 @@ def fileread():
 # scoring urls function
 def scoreurl(url):
     urlscore = 5
+
+    #check for https
     if ("https" in url):
-        urlscore += 1
+        urlscore += 2
 
+    #check for http
+    if ("http" in url):
+        urlscore += 2
 
+    #checks for .com, .org, 
+
+    #checks for .html
+    if ("html" in url):
+        urlscore -= 2
     print(urlscore)
-
 
 # main function
 def main():
-
     data = fileread()
-    for row in data:
-        scoreurl(row[1])
     
+    for i, row in data:
+        if ((i % 2 == 0) and (i < 52)) :
+            scoreurl(row)
+            print(row)
+            i += 1
+        else:
+            print(row)
+            i += 1
+       
     # printing entries 1-20
     # print(data.head(21))
     
     # sorting dataframe
     # data.sort_values("type", axis=0, ascending=False, inplace=True)
     
-
 if __name__ == "__main__":
-
     main()
